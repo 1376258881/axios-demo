cd08aea3efb9a45a7d811936ef6f375abeafded0
汪新宇 <5430365+wang-xinyu@user.noreply.gitee.com>
2020-05-30 10:01:25 +0800

    init axios

---
+++ aap.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+      <div class='container'></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"",
+  components: {},
+  props: {},
+  data() {
+    return {
+
+    };
+  },
+  created() {},
+  mounted() {},
+  activited() {},
+  update() {},
+  beforeRouteUpdate() {},
+  methods: {
+
+  },
+  filter: {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
